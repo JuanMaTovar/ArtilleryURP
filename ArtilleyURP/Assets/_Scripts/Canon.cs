4f8d876376a7d3daeacf5e53c04d36deeda52fe2
JuanMaTovar <juan.tovar@utnay.edu.mx>
2023-12-29 01:02:00 -0700

    Actividad_modulo_17
    Realización de la actividad del modulo 17

--- ArtilleyURP/Assets/_Scripts/Canon.cs
+++ ArtilleyURP/Assets/_Scripts/Canon.cs
@@ -4,7 +4,14 @@ using UnityEngine;
 
 public class Canon : MonoBehaviour
 {
+    public static bool Bloqueado;
+
+    public AudioClip clipDisparo;
+    private GameObject SonidoDisparo;
+    private AudioSource SourceDisparo;
+
     [SerializeField] private GameObject BalaPrefab;
+    public GameObject ParticulasDisparo;
     private GameObject puntaCanon;
     private float rotacion;
 
@@ -12,11 +19,14 @@ public class Canon : MonoBehaviour
     void Start()
     {
         puntaCanon = transform.Find("PuntaCanon").gameObject;
+        SonidoDisparo = GameObject.Find("SonidoDisparo");
+        SourceDisparo = SonidoDisparo.GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        
         rotacion += Input.GetAxis("Horizontal") * AdministradorJuego.VelocidadRotacion;
         if(rotacion <= 90 && rotacion >= 0)
         {
@@ -25,13 +35,20 @@ public class Canon : MonoBehaviour
         if (rotacion > 90) rotacion = 90;
         if (rotacion < 0) rotacion = 0;
 
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && !Bloqueado)
         {
             GameObject temp = Instantiate(BalaPrefab, puntaCanon.transform.position, transform.rotation);
+           
             Rigidbody tempRB = temp.GetComponent<Rigidbody>();
+            SeguirCamara.objetivo = temp;
             Vector3 direccionDisparo = transform.rotation.eulerAngles;
             direccionDisparo.y = 90 - direccionDisparo.x;
+            Vector3 direccionParticulas = new Vector3(-90 + direccionDisparo.x, 90, 0);
+            GameObject Particulas = Instantiate(ParticulasDisparo, puntaCanon.transform.position, Quaternion.Euler(direccionParticulas), transform);
             tempRB.velocity = direccionDisparo.normalized * AdministradorJuego.VelocidadBala;
+            SourceDisparo.Play();
+            //SourceDisparo.PlayOneShot(clipDisparo);
+            Bloqueado = true;
         }
         
     }
