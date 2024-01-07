20b95695f1a9be35a002c9c740aefd9f3ba9ff44
JuanMaTovar <juan.tovar@utnay.edu.mx>
2024-01-06 18:40:09 -0700

    modulo 18a
    Actualización del modulo 18 antes de realizar la actividad de evaluación

--- ArtilleyURP/Assets/_Scripts/AdministradorJuego.cs
+++ ArtilleyURP/Assets/_Scripts/AdministradorJuego.cs
@@ -6,8 +6,11 @@ public class AdministradorJuego : MonoBehaviour
 {
     public static AdministradorJuego SingletonAdministradorJuego;
     public static int VelocidadBala = 30;
-    public static int DisparosPorJuego = 10;
+    public static int DisparosPorJuego = 3;
     public static float VelocidadRotacion = 1;
+
+    public GameObject CanvasGanar;
+    public GameObject CanvasPerder;
 
     private void Awake()
     {
@@ -29,6 +32,19 @@ public class AdministradorJuego : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(DisparosPorJuego < 0)
+        {
+            PerderJuego();
+        }
+    }
+
+    public void GanarJuego()
+    {
+        CanvasGanar.SetActive(true);
+    }
+
+    public void PerderJuego()
+    {
+        CanvasPerder.SetActive(true);
     }
 }
